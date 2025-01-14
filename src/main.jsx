7640540068faf88b1d660658a85ae84a992addc0
Rehan-013 <153925483+Rehan-013@users.noreply.github.com>
2025-01-14 12:07:10 +0530

    Vercel Analytics Added

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import router from './routers/routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <RouterProvider router={router}/>
     </HelmetProvider>
+    <Analytics />
   </StrictMode>,
 );
