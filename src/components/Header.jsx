6fa8d4e13c12ae3698f0ae7e16b8fd6effaeac4c
Rehan-013 <153925483+Rehan-013@users.noreply.github.com>
2025-01-14 12:03:24 +0530

    Logo Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { Button, IconBtn } from './Button';
 import { Link } from 'react-router-dom';
 import { useToggle } from '../hooks/useToggle';
 import MenuItems from './MenuItems';
+import { logo } from '../assets/assests.js';
 const Header = () => {
 
   const [isOpen, toggleIsOpen] = useToggle(false);
 
   return (
     <div className='header'>
-      <Link className='left' to="/">COMPANY LOGO</Link>
+      <Link className='left' to="/"><img src={logo} alt="logo"/></Link>
       <div className="right">
         <Link to="/contact" className='hidden'>
           <Button className='nav_button' children="Contact Us" />
